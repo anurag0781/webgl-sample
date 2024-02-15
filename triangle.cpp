f35a39d6abd3b960645ec2b6f998a887edb4041c
Prem Kumar <pkshankals@gmail.com>
2024-02-15 12:53:49 +0530

    changes to access canvases and context on worker thread

--- triangle.cpp
+++ triangle.cpp
@@ -9,6 +9,10 @@
 #include <emscripten/threading.h>
 #include <pthread.h>
 #include <emscripten/html5_webgpu.h>
+<<<<<<< Updated upstream
+=======
+#include <string>
+>>>>>>> Stashed changes
 
 pthread_t renderingThreadId = 0, render_th_id = 0;
 
@@ -111,13 +115,80 @@ void render() {
 
 void processPendingJobs()
 {
+<<<<<<< Updated upstream
     printf("\n\nInside worker thread.\n\n");
+=======
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx = emscripten_webgl_get_current_context();
+    // printf("\n\nInside worker thread.\n\n");
+    EM_ASM({
+        // var gl = Emval.toValue($0);
+        // console.log(gl);
+        var gl = Module.GL.currentContext.GLctx;
+        // var gl = Module.GL.getContext($0).GLctx;
+        // var canvas = Module.findCanvasEventTarget('#webgl_canvas');
+        // var gl = canvas.getContext('webgl');
+        gl.clearColor(0.0, 0.0, 1.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // var webgpuContext = Module.findCanvasEventTarget('#webgpu_canvas').offscreenCanvas.getContext('webgl');
+        // webgpuContext.clearColor(0.0, 1.0, 0.0, 1.0);
+        // webgpuContext.clear(gl.COLOR_BUFFER_BIT);
+
+        var webgpuContext = Module.findCanvasEventTarget('#webgpu_canvas').offscreenCanvas.getContext('2d');
+        var width = Module.findCanvasEventTarget('#webgpu_canvas').offscreenCanvas.width;
+        var height = Module.findCanvasEventTarget('#webgpu_canvas').offscreenCanvas.height;
+        webgpuContext.fillStyle = 'red';
+        webgpuContext.fillRect(0, 0, width, height);
+
+        
+        
+    }, ctx);
+
+    
+
+>>>>>>> Stashed changes
 }
 
 void *renderingThreadEntry(void *arg)
 {
     render_th_id = pthread_self();
     printf("\n\nInside renderingThreadEntry().\n\n");
+<<<<<<< Updated upstream
+=======
+    EM_ASM({
+        // console.log("Inside processPendingJobs().1");
+        // const offscreen = new OffscreenCanvas(256, 256);
+        // offscreen.id = "webgpu_canvas";
+        // var webgpuContext = offscreen.getContext('webgpu');
+        // console.log(webgpuContext);
+    });
+    // webgpu calls which uses webgpu_canvas
+
+    std::string jsCanvasSelector = "#webgl_canvas";
+    EmscriptenWebGLContextAttributes attrs;
+    emscripten_webgl_init_context_attributes(&attrs);
+    attrs.enableExtensionsByDefault = 1;
+    attrs.powerPreference = EM_WEBGL_POWER_PREFERENCE_HIGH_PERFORMANCE;
+    attrs.proxyContextToMainThread = EMSCRIPTEN_WEBGL_CONTEXT_PROXY_DISALLOW;
+    attrs.depth = 1;
+    attrs.stencil = 1;
+    attrs.antialias = 1;
+    attrs.majorVersion = 2;
+    attrs.minorVersion = 0;
+    attrs.premultipliedAlpha = true;
+    
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx = emscripten_webgl_create_context(jsCanvasSelector.c_str(), &attrs);
+    if (ctx > 0)
+    {
+        printf("\n\nCreation of context succeeded in the worker!!!\n\n");
+    }
+    else
+    {
+         printf("\n\nCreation of context failed in the worker!!!\n\n");
+    }
+    emscripten_webgl_make_context_current(ctx);
+
+>>>>>>> Stashed changes
     emscripten_set_main_loop(processPendingJobs, 60, 1);
     return nullptr;
 }
@@ -126,10 +197,16 @@ int main()
 {
     pthread_attr_t attr;
     pthread_attr_init(&attr);
+<<<<<<< Updated upstream
     emscripten_pthread_attr_settransferredcanvases(&attr, "#webgpu_canvas2");
     pthread_create(&renderingThreadId, &attr, renderingThreadEntry, (void *)nullptr);
 
 
+=======
+    emscripten_pthread_attr_settransferredcanvases(&attr, "#webgl_canvas,#webgpu_canvas");
+    pthread_create(&renderingThreadId, &attr, renderingThreadEntry, (void *)nullptr);
+
+>>>>>>> Stashed changes
     init();
     emscripten_set_main_loop(render, 0, 1);
 
